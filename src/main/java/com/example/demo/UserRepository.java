620618fb567370efa8f04935781fb89afa6d61f4
SuperFunk <jsmirnakis@gmail.com>
2024-01-20 10:08:52 +0200

    .

---
+++ src/main/java/com/example/demo/UserRepository.java
@@ -0,0 +1,10 @@
+package main.java.com.example.demo;
+
+import com.example.demo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
